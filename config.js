4e1c5095e98c2f8626d078bb10ffbf181673d6cb
Fábio Sakamoto <fabiosakamotoma@gmail.com>
2020-07-08 12:41:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,14 @@
 */
 
 var settings = {
-    'radio_name': 'Brasil Hits',
+    'radio_name': 'NoPlayer Music',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'https://streaming.brasilhits.com/acc',
+    'url_streaming': 'https://stm6-ssl.painelcast.com:6934',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
-    'streaming_type': 'icecast',
+    'streaming_type': 'shoutcast',
 
     // Visit https://api.vagalume.com.br/docs/ to get your API key
     'api_key': '18fe07917957c289983464588aabddfb',
