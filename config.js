60c3e2f6c9bed2a9daae3caca14312c7bdc26731
Fábio Sakamoto <fabiosakamotoma@gmail.com>
2024-02-17 21:41:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var settings = {
     'radio_name': 'NoPlayer Music',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'https://ssl.painelcast.com:6926',
+    'url_streaming': 'https://stm3.painelcast.com:7070',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
